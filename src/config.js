8e7c896b5c80c8eb67aedba5222123c485676c5a
NicRiv <nico.a.rivera3@gmail.com>
2022-10-08 16:50:54 -0300

    database variables

--- src/config.js
+++ src/config.js
@@ -4,3 +4,10 @@ config()
 
 // Server
 export const PORT = process.env.PORT || 3000
+
+// Database
+export const DB_HOST = process.env.DB_HOST || 'localhost'
+export const DB_PORT = process.env.DB_PORT || 3306
+export const DB_DATABASE = process.env.DB_DATABASE || 'quiz'
+export const DB_USERNAME = process.env.DB_USERNAME || 'root'
+export const DB_PASSWORD = process.env.DB_PASSWORD || ''
