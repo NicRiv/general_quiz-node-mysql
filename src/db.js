dacbcdba0a02dc8ea353c15336bdc9cc8aedadc5
NicRiv <nico.a.rivera3@gmail.com>
2022-10-08 16:51:15 -0300

    file db.js added

---
+++ src/db.js
@@ -0,0 +1,17 @@
+import { createPool } from 'mysql2/promise'
+import {
+    DB_DATABASE,
+    DB_HOST,
+    DB_PASSWORD,
+    DB_PORT,
+    DB_USERNAME
+} from './config.js'
+
+// Connection to MySQL
+export const pool = createPool({
+    host: DB_HOST,
+    port: DB_PORT,
+    user: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: DB_DATABASE,
+})
