88309d79c64817447892039c5d1bab0718c179dc
NicRiv <nico.a.rivera3@gmail.com>
2022-10-09 23:22:37 -0300

    create quiz table

---
+++ src/database/migrations/create_quiz_table.js
@@ -0,0 +1,38 @@
+/**
+ * Create Quiz table
+ */
+
+import { pool } from '../db.js'
+
+// Create table
+(async () => {
+    try {
+        // Drop table
+        await pool.query('DROP TABLE IF EXISTS quiz')
+
+        // Create
+        const [res] = await pool.query(`
+            CREATE TABLE IF NOT EXISTS quiz(
+                id INT(11) NOT NULL AUTO_INCREMENT, 
+                description VARCHAR(45) DEFAULT NULL,
+                q1 VARCHAR(90) DEFAULT NULL,
+                q2 VARCHAR(90) DEFAULT NULL,
+                q3 VARCHAR(90) DEFAULT NULL,
+                q4 VARCHAR(90) DEFAULT NULL,
+                PRIMARY KEY(id)
+            ) 
+        `)
+        // console.log(res)
+
+        if (res.warningStatus === 0) {
+            console.log('Table created')
+        } else {
+            console.log('The table is already exists')
+        }
+
+        process.exit(0)
+    } catch (error) {
+        console.log(error)
+        process.exit(1)
+    }
+})()
